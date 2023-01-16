a3e773d3630bc42a33e805f7445b78bfeb2e72e0
anita0007 <114204165+anita0007@users.noreply.github.com>
2023-01-16 21:01:46 +0800

    Add files via upload

--- Time.java
+++ Time.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 public class Time{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("請輸入台灣現在的時間: ");
+        System.out.println("請輸入台灣現在的時間: 點");
         int a = input.nextInt();
+        System.out.println("請輸入台灣現在的時間: 分");
+        int b = input.nextInt();
         int time = (a + 9)%24;
-        System.out.println("美國現在時間為: "+time);
+        System.out.printf("美國現在時間為:"+"%d" +"點"+"%d"+"分",time,b);   
     }
 }
             
